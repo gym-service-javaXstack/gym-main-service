94baa58e15f01b093d3e5fc1cbac53844164e652
m-sobur <msobur123@gmail.com>
2024-03-18 19:46:35 +0200

    Small fixes

--- src/main/java/com/example/springcore/dto/TrainerDTO.java
+++ src/main/java/com/example/springcore/dto/TrainerDTO.java
@@ -1,6 +1,5 @@
 package com.example.springcore.dto;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -9,9 +8,8 @@ import lombok.ToString;
 @Data
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class TrainerDTO extends UserDTO{
+public class TrainerDTO extends UserDTO {
 
-    @Valid
     @NotNull
     private TrainingTypeDTO specialization;
 
--- src/main/java/com/example/springcore/dto/UserDTO.java
+++ src/main/java/com/example/springcore/dto/UserDTO.java
@@ -1,7 +1,6 @@
 package com.example.springcore.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
@@ -9,19 +8,15 @@ import lombok.Data;
 public class UserDTO {
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    @Schema(description = "User userName (optional)")
     private String userName;
 
     @NotEmpty
-    @Schema(description = "User firstName (required)")
     private String firstName;
 
     @NotEmpty
-    @Schema(description = "User lastName (required)")
     private String lastName;
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    @Schema(description = "User isActive (optional)")
     private Boolean isActive;
 
 }
