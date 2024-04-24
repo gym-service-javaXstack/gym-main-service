af21b104a407a139652a2160c27facbd2bf5edda
m-sobur <msobur123@gmail.com>
2024-04-24 16:58:53 +0300

    Added circuit breaker for 500 exceptions from feign client

--- src/main/java/com/example/springcore/feign/GymReportsClient.java
+++ src/main/java/com/example/springcore/feign/GymReportsClient.java
@@ -2,6 +2,7 @@ package com.example.springcore.feign;
 
 import com.example.springcore.config.FeignConfig;
 import com.example.springcore.dto.TrainerWorkLoadRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -14,16 +15,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @FeignClient(name = "gym-report", configuration = FeignConfig.class)
 @Component
 public interface GymReportsClient {
 
     @PostMapping("/api/v1/workload")
+    @CircuitBreaker(name = "gym-report")
     ResponseEntity<Void> processTrainerWorkload(
             @RequestBody TrainerWorkLoadRequest request
     );
 
+    @PostMapping("/api/v1/workload/list")
+    @CircuitBreaker(name = "gym-report")
+    ResponseEntity<Void> processListTrainerWorkload(
+            @RequestBody List<TrainerWorkLoadRequest> request
+    );
+
     @GetMapping("/api/v1/workload")
+    @CircuitBreaker(name = "gym-report")
     Integer getTrainerSummaryByUsername(
             @RequestParam(name = "username") String username,
             @RequestParam(name = "year") int year,
